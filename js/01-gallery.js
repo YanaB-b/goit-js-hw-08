cc6372d9b3d06cbb0012c815850d0fa93aecc8d2
YanaB-b <j.biliuk96@gmail.com>
2023-01-30 11:01:05 +0100

    q

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryBoxRef = document.querySelector('.gallery');
+const galleryBoxMarkup = createBoxGallery(galleryItems);
+galleryBoxRef.insertAdjacentHTML('afterbegin',galleryBoxMarkup);
+
+function createBoxGallery(galleryItems){
+    return galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`)
+  .join('');
+}
+
+const galleryBoxModal = new SimpleLightBox('.gallery a',{
+    captionsData : 'alt',
+    captionsDelay: 300,
+})
